4f557e9f30b06d6f375584c0d939f6c70b67f901
Ethan Rowe <redsoxrowe@gmail.com>
2018-06-05 13:41:53 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean hasADog = false;
+
+        char favoriteLetter = 'c';
+
+        String weather = "It is a hot day today.";
+
+        double averaveGpa;
+        averaveGpa = 1232.002;
+
+        int favoriteNumber = 750;
+
+        int dayOfWeek;
+
+        System.out.println(x:int numberOfStudentInClassroom) = 1232;
+        age + (numberOfStudentInClassroom);
+
+        int add = 5 + 4;
+        int subtract = 5 - 4;
+        int multiply = 5 * 4;
+        int divide = 4.0 / 5;
+
+            System.out.println ("The time of the day is " + (5 - 2));
+
+            System.out.println("This is the modulo" + (4 % 5));
+
+            String hello = "Hello";
+            String name = "Ethan";
+
+            System.out.println(hello + name);
+
+            name = input.nextline();
+
+
+    }
+}
